a5ab2be998c534c55bf372f1f99ead5273456993
Sanjay Vaghela <Sanjay_Vaghela@mckinsey.com>
2015-02-07 21:36:33 +0530

    common utils

---
+++ src/utils.py
@@ -0,0 +1,56 @@
+'''
+Notes: Keep all COMMON UTIL functions here. Repeat, "COMMON UTIL"
+'''
+import os, sys
+import time, datetime
+import logging
+
+## Returns filename in the format of "[basename][date/timestamp][sep][suffix]"
+# @param basename a string, for basename to be used
+# @param suffix a string, for suffix to be used as a file extension
+# @param sep a string, for separator
+# @param datestamp a boolean, for enabling datestamp with basename
+# @param timestamp a boolean, for enabling timestamp with basename
+def formatted_filepath(basename='', suffix='', sep='', datestamp=False, timestamp=False):
+    '''
+    Returns filename in the format of "[basename][date/timestamp][sep][suffix]"
+    Example#1
+    >>> formatted_filepath('utils', 'log', '.')
+    'utils.log'
+    '''
+    basename = basename or "%s"%(sys.argv[0].split(os.path.extsep,1)[0])
+    if timestamp:
+        basename += "_%s"%( datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%dT%H%M%S") )
+    elif datestamp:
+        basename += "_%s"%( datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d") )
+    return "%s%s%s"%(basename, sep, suffix)
+
+## Return python logger
+# @param filename a string, for logger filename
+# @param format a string, for logging format. Must comply with logging format
+# @param level an int, for setting log level to DEBUG/INFO/ERROR
+def get_logger(filename='', format="%(asctime)s: %(levelname)s: %(message)s", level=logging.DEBUG):
+    filename = filename or formatted_filepath('', 'log', '.')
+    logging.basicConfig(filename=filename, format="%(asctime)s: %(levelname)s: %(message)s", level=logging.DEBUG)
+    return logging.getLogger(filename)
+
+## raises exception with given error message
+# @param err_msg error message to display
+def raise_error_msg(err_msg, *kwds):
+    '''
+    Example:
+    >>> raise_error_msg('Hello World!')
+    Traceback (most recent call last):
+       ...
+    ValueError: Hello World!
+
+    >>> raise_error_msg('Hello %s%s%s%s%s!', 'W', 'O', 'R', 'L', 'D')
+    Traceback (most recent call last):
+       ...
+    ValueError: Hello WORLD!
+    '''
+    if kwds:
+        raise ValueError(err_msg%(kwds))
+    else:
+        raise ValueError(err_msg)
+
