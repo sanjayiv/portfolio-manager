3fd959e940951fe9f074fee681a8397c81df81ac
Sanjay Vaghela <Sanjay_Vaghela@mckinsey.com>
2015-02-28 19:57:48 +0530

    added graceful_exit and pretty_log functions

--- src/utils.py
+++ src/utils.py
@@ -54,3 +54,22 @@ def raise_error_msg(err_msg, *kwds):
     else:
         raise ValueError(err_msg)
 
+## gracefully exit the execution after showing error message
+# @param warning_msg warning message to display
+# @param kwds all other keyword arguments to warning_msg
+def graceful_exit(warning_msg, *kwds):
+    if kwds:
+        print(warning_msg%(kwds))
+    else:
+        print(warning_msg)
+    sys.exit(0)
+
+## return pretty log message for given message
+# @param message a text, for logging or rendering
+def pretty_log(message, line_padding=0, decorator="-"):
+    return "\n"*(line_padding) + \
+            decorator*(4+len(message)) + \
+            "\n| " + message + " |\n" + \
+            decorator*(4+len(message)) + \
+            "\n"*line_padding
+
